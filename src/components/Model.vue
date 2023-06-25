11d49fb9b661496394898d7ba0c4937006bef5d5
wjjwkwindy <wjjwkwindy@gmail.com>
2023-06-25 16:56:19 +0800

    chore: 优化弹窗样式

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="fixed top-0 bottom-0 right-0 left-0 z-40" :class="{'pointer-events-none':!value}">
-    <div class="bg-white opacity-80 top-0 bottom-0 right-0 left-0 absolute" :class="{'opacity-0':!value}" @click="setValue(false)"></div>
-    <div class="bg-white absolute bottom-0 left-0 right-0" :style="getStyle()">
+  <div class="fixed top-0 bottom-0 right-0 left-0 z-40" :class="{ 'pointer-events-none': !value }">
+    <div
+      class="bg-white top-0 bottom-0 right-0 left-0 absolute transition-opacity duration-500 ease-out"
+      :class="[value ? 'opacity-80' : 'opacity-0']"
+      @click="setValue(false)"
+    ></div>
+    <div
+      class="bg-white absolute bottom-0 left-0 right-0 transition-all duration-200 ease-out"
+      :style="[value ? '' : 'transform: translateY(100%)']"
+    >
       <slot></slot>
     </div>
   </div>
@@ -12,15 +19,6 @@ const props = defineProps({
   value: Boolean,
   setValue: Function,
 });
-console.log(props.setValue);
-
-function getStyle() {
-  if (props.value) {
-    return '';
-  } else {
-    return 'transform: translateY(100%);';
-  }
-}
 </script>
 
 <style lang="scss" scoped></style>
